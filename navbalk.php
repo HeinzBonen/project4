4649196a1637d485db6d51bfd262ffae194310f3
MerijnHNE <Merijnvanhettema@hotmail.nl>
2022-12-21 13:07:03 +0100

    Aanpassingen css en php bestand - Navigatie balk - Aanpassingen schriftelijke bestanden georderd.

---
+++ navbalk.php
@@ -0,0 +1,19 @@
+<!--Configuratie HTML, CSS talen-->
+<!DOCTYPE html>
+<html lang="nl">
+<body></body>
+
+<!--Importeren CSS bestand navigatiebalk-->
+<link rel="stylesheet" href="css_bestanden/navbalk.css">
+
+<!--Plaatsen & Configureren van de applicatie balk-->
+<div>
+    <nav>
+        <a class="navloguit" href="/">Log uit</a>
+    </nav>
+</div>
+<br><br><br>
+<hr>
+<br><br>
+
+</html>

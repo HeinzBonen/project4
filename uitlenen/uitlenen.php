a56d98e26d9da6c017553d73c487cb97937a984b
Operator220 <243279@edu.rocfriesepoort.nl>
2023-01-10 19:21:53 +0100

    uitlenen

--- uitlenen/uitlenen.php
+++ uitlenen/uitlenen.php
@@ -4,7 +4,7 @@ session_start();
 //database connectie
 include("../db.php");
 
-$query = "SELECT pnaam, pmerk, pserienummer, ptype, pstaat, pstatus FROM producten ";
+$query = "SELECT ID, pnaam, pmerk, pserienummer, ptype, pstaat, pstatus FROM producten ";
 
 ?>
 <!doctype html>
@@ -30,21 +30,27 @@ $query = "SELECT pnaam, pmerk, pserienummer, ptype, pstaat, pstatus FROM product
         <hr>
    </div>
 
-   <div style="float: right; padding: 4%; margin-right: 10%; margin-top: 5%; border-style: solid;" >
+   <div style="z-index: 9; float: right; padding: 4%; margin-right: 10%; margin-top: 5%; border-style: solid;" >
      <h3>Uitlenen Producten</h3>
      <br>
      <form action="index.html" method="post">
-       <label for="serienummer">Serienummer product:</label><br>
-         <input type="text" id="serienummer" name="serienummer" placeholder="MB3453s234" value=""><br>
+       <label for="id">ID Product:</label><br>
+         <input type="text" id="id" name="id" placeholder="12" value=""><br><br>
          <label for="studentnummer">Studentnummer:</label><br>
          <input type="text" id="studentnummer" name="studentnummer" placeholder="278556" value=""><br><br>
-         <input type="submit" value="Submit">
+         <label for="duitleen">Datum Uitleen:</label><br>
+         <input type="date" id="duitleen" name="duitleen" value=""><br><br>
+         <label for="dretour">Datum Retour:</label><br>
+         <input type="date" id="dretou" name="dretou" value=""><br><br>
+         <label for="reduitleen">Reden Uitleen:</label><br>
+         <input type="text" id="reduitleen" name="reduitleen" placeholder="Reden Uitleen" value=""><br><br>
+         <input type="submit" value="Lening Toewijzen">
      </form>
    </div>
 
    <table style="left: 0; top: 10%; width: 30%; max-width: 30%; ">
      <tr class="top"><th>Beschikbare Producten</th></tr>
-     <tr class="top"><th>Naam</th> <th>Merk</th> <th>Type</th> <th>Serienummer</th> <th>Staat</th></tr>
+     <tr class="top"><th>ID</th> <th>Naam</th> <th>Merk</th> <th>Type</th> <th>Serienummer</th> <th>Staat</th></tr>
    <?php
    //admin email list
 
@@ -52,7 +58,7 @@ $query = "SELECT pnaam, pmerk, pserienummer, ptype, pstaat, pstatus FROM product
 
    while($row=$r_set->fetch_assoc()) {
      if($row["pstatus"] == "Beschikbaar"){
-     echo "<tr><th>$row[pnaam]</th> <th>$row[pmerk]</th> <th>$row[ptype]</th> <th>$row[pserienummer]</th> <th>$row[pstaat]</th></tr>";
+     echo "<tr><th>$row[ID]</th> <th>$row[pnaam]</th> <th>$row[pmerk]</th> <th>$row[ptype]</th> <th>$row[pserienummer]</th> <th>$row[pstaat]</th></tr>";
    }
   }
    }
@@ -69,7 +75,7 @@ $query = "SELECT pnaam, pmerk, pserienummer, ptype, pstaat, pstatus FROM product
 
 
 
-      <table style="right: 26%; top: 55%; width: 20%; max-width: 20%;">
+      <table style="right: 26%; top: 97%; width: 20%; max-width: 20%;">
         <tr class="top"><th>Uitgeleende Producten</th></tr>
         <tr class="top"><th>Naam</th> <th>Merk</th> <th>Type</th> <th>Serienummer</th></tr>
       <?php
@@ -86,7 +92,7 @@ $query = "SELECT pnaam, pmerk, pserienummer, ptype, pstaat, pstatus FROM product
       ?>
  </table>
 
- <table style="right: 1%; top: 55%; width: 15%; max-width: 15%;">
+ <table style="right: 1%; top: 97%; width: 15%; max-width: 15%;">
    <tr class="top"><th>Uitgeleende Producten</th></tr>
    <tr class="top"><th>Leerlingnummer</th> <th>Naam</th> <th>Type</th></tr>
  <?php
